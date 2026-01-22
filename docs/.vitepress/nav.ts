88ad89f65844fc57b1b7880e11e243a0b36a2d43
SLcodep <2483548146@qq.com>
2026-01-22 21:46:55 +0800

    first commit

---
+++ docs/.vitepress/nav.ts
@@ -0,0 +1,31 @@
+// .vitepress/navConfig.ts
+import type { DefaultTheme } from "vitepress";
+
+// 定义导航配置（复用 VitePress 内置类型，保证类型提示）
+export const nav: DefaultTheme.NavItem[] = [
+  { text: "首页", link: "/" },
+  {
+    text: "前端基础",
+    items: [
+      { text: "javascript", link: "/javascript/01" },
+      { text: "css", link: "/css/01" },
+    ],
+  },
+  {
+    text: "Vue",
+    link: "/vue/start",
+    activeMatch: "/10.Vue/",
+  },
+  {
+    text: "功能页",
+    items: [
+      { text: "归档页", link: "/archives" },
+      { text: "清单页", link: "/articleOverview" },
+      { text: "登录页", link: "/login" },
+      {
+        text: "风险链接提示页",
+        link: "/risk-link?target=https://vp.teek.top",
+      },
+    ],
+  },
+];
